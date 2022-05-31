5b952cab3af5742a5e88d1a745aa7b1fca5fd3d0
tommie1236 <timooosterom@gmail.com>
2022-05-31 11:26:47 +0200

    updated some code

--- mapi.py
+++ mapi.py
@@ -1,5 +1,7 @@
+# imports
 from selenium import webdriver
-driver = webdriver.Chrome() 
-driver.get("http://www.google.com")
-for elem in driver.find_elements_by_xpath('.//span[@class = "gbts"]'):
-    print(elem.text)
+from data import SCHOOL_URL
+
+# code
+driver = webdriver.Chrome()
+webdriver.get(SCHOOL_URL)
