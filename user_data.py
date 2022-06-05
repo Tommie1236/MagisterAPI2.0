00b6cad32c5452a313184b2d4ead1508f24515dc
Tommie1236 <90513578+Tommie1236@users.noreply.github.com>
2022-06-05 09:37:32 +0200

    Create user_data.py
    this is a config file where you put you data to log into magister

---
+++ user_data.py
@@ -0,0 +1,4 @@
+SCHOOL_URL = ''  # dont forget the 'https://' at the begin, otherwise it wont work. example 'https://passie.magister.net'
+STUDENT_NUMBER = 00000  # your student number
+STUDENT_PASSWORD = '' # your magister password 
+MAX_COURSES = 10    # max amount of courses possible in magister (the number number in magister before your course)
